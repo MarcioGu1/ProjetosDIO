4917da96d3b9de2fca55c56c1c34e0491d6bc10d
Ret <marciogel09@gmail.com>
2024-07-17 13:18:24 -0300

    atualização

--- Machine Learning  na AWS/assistente_virtual.py
+++ Machine Learning  na AWS/assistente_virtual.py
@@ -6,15 +6,16 @@ e realizar comando no sistema operaconal
 
 #importando bibliotecas
 from selenium import *
+import pyautogui as py
 import pyttsx3
-import subprocess
+
 import os 
 import speech_recognition as sr
 import time
 
 class Assistent:
     def __init__(self):
-        self.falar("Bom dia,sou Friday sua assistente e irei te atender hoje")
+        self.falar("oi")
 
     # Função para o Friday  falar
     def falar(self,frase):
@@ -40,10 +41,10 @@ class Assistent:
             return audio,microfone
     def tarefas(self,audio,microfone):        
         try:
-            
+
             #passa o comando para o algoritimo interpretar
             frase = microfone.recognize_google(audio,language = "pt-BR")
-
+            
             self.falar(f" Mister o senhor disse {frase} ")
             print(f'{frase}')
             
@@ -51,9 +52,47 @@ class Assistent:
                 os.system("start Chrome.exe")
                 self.falar("Abrindo o google,mais alguma coisa ?")
             
-            elif "Excel" in frase :
-                os.system("start Excel.exe")
+            elif "Gamer" in frase :
+                self.falar("senhor qual jogo você deja jogar agora")
+                try:
+                    self.ouvir_microfone()
+                    frase = microfone.recognize_google(audio,language = "pt-BR")
                     
+                    if "Valorant" in frase :
+                        py.press("win")
+                        py.write("valorant")
+                        py.press("enter")
+                    elif"Cs" in frase:
+                        pass
+                    elif "Gta" in frase:
+                            try:
+                                # Caminho da pasta e do aplicativo
+                                caminho_pasta = r'C:\Users\Home\Desktop\DIO'
+                                caminho_aplicativo = os.path.join(caminho_pasta, 'Cherax.exe')
+                                
+                                # Abre a pasta (Opcional)
+                                os.startfile(caminho_pasta)
+                                time.sleep(2)  # Espera a pasta abrir
+
+                               # Abre o aplicativo
+                                py.doubleClick(x=365 , y =596)
+                                time.sleep(1)
+                                #clica no botao login
+                                py.click(x=551,y=497)
+                                time.sleep(3)
+                                #leva o cursor e depois clica para iniciar
+                                py.click(x=220,y=358)
+                                py.doubleClick(x=220, y=358)
+                                time.sleep(3)
+                                # aceita o termo de segurança
+                                time.sleep(5)
+                                py.click(x=815, y=791)
+                                py.click(x=927 , y=346)
+                            except Exception as e:
+                                print(f"Erro ao abrir o aplicativo: {e}")
+                except sr.UnknownValueError:
+                    print("Não entendi mestre")
+                    return False
             elif "PowerPoint" in frase:
                 os.system("start POWERPNT.exe")
                 self.falar("Abrindo o google,mais alguma coisa ?")
